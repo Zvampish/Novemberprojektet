5d02661f014ffe12ac04875641239129b0cf30f9
Zvampish <nornostvold02@gmail.com>
2020-11-18 10:52:29 +0100

    Klistrade in tidigare Pong grund som jag själv skrivit

--- Novemberprojektet/Program.cs
+++ Novemberprojektet/Program.cs
@@ -17,7 +17,68 @@ namespace Novemberprojektet
             // Klassdiagram []
             // Synlighet (public/private/protected) []
 
+            int windowSize = 800;
+            int rectangleSizeX = 20;
+            int rectangleSizeY = 125;
+
+            Raylib.InitWindow(800, 800, "Hello Man!");
+
+            Random generator = new Random();
+
+            float playerOneX = 20;
+            float playerOneY = 325;
+            float playerTwoX = 760;
+            float playerTwoY = 325;
+
+            while(!Raylib.WindowShouldClose())
+            {
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+                {
+                    playerOneY -= .2f;
+                }
+                else if (Raylib.IsKeyDown(KeyboardKey.KEY_Z))
+                {
+                    playerOneY += .2f;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_K))
+                {
+                    playerTwoY -= .2f;
+                }
+                else if (Raylib.IsKeyDown(KeyboardKey.KEY_M))
+                {
+                    playerTwoY += .2f;
+                }
+
+                if(playerOneY < windowSize - windowSize)
+                {
+                    playerOneY += .2f;
+                }
+                else if(playerOneY > windowSize - rectangleSizeY)
+                {
+                    playerOneY -=.2f;
+                }
+
+                if(playerTwoY < windowSize - windowSize)
+                {
+                    playerTwoY += .2f;
+                }
+                else if(playerTwoY > windowSize - rectangleSizeY)
+                {
+                    playerTwoY -=.2f;
+                }
+
+                Raylib.BeginDrawing();
+
+                Raylib.ClearBackground(Color.BLACK);
+
+                Raylib.DrawRectangle((int)playerOneX,(int)playerOneY, rectangleSizeX, rectangleSizeY, Color.WHITE);
+
+                Raylib.DrawRectangle((int)playerTwoX,(int)playerTwoY, rectangleSizeX, rectangleSizeY, Color.WHITE);
+
+                Raylib.EndDrawing();
             
+            }
         }
     }
 }
